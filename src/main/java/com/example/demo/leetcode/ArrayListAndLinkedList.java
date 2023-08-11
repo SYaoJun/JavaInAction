d49e117e3c5f5110e63b3bfd7931161f040d1d52
yaojun <940334249@qq.com>
2023-08-11 21:11:23 +0800

    fix: readme

---
+++ src/main/java/com/example/demo/leetcode/ArrayListAndLinkedList.java
@@ -0,0 +1,12 @@
+package com.example.demo.leetcode;
+
+import java.util.ArrayList;
+
+public class ArrayListAndLinkedList {
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<>();
+        integers.add(100);
+        integers.add(200);
+        integers.add(300);
+    }
+}
--- src/main/java/com/example/demo/leetcode/LC2022.java
+++ src/main/java/com/example/demo/leetcode/LC2022.java
@@ -2,6 +2,6 @@ package com.example.demo.leetcode;
 
 public class LC2022 {
     public static void main(String[] args) {
-
+        System.out.println("hello world");
     }
 }
